30959080cd281f6f7d145a8511f8f2cd1dfe5b3d
Alex Van Camp <vancamp.alex@gmail.com>
2020-02-28 17:49:26 -0600

    fix: install prod deps

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 git clone https://github.com/nodecg/nodecg.git
 cd nodecg
-npm i
+npm i --production
 cd bundles
 git clone https://github.com/EwanLyon/tutorial-bundle.git
 cd tutorial-bundle
-npm i
+npm i --production
